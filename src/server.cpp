79fa70c07f5a5dc46c428bb4046a7b8b26e26545
Landon94 <lkarkut@uci.edu>
2025-05-12 16:48:24 -0700

    Added parse function to parse http request. Added more server responses .

--- src/server.cpp
+++ src/server.cpp
@@ -7,6 +7,7 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <sstream>
+#include <unordered_map>
 
 
 #define BACKLOG 10
@@ -15,12 +16,11 @@ std::string messageStr(char* buf, int size) {
     return std::string(buf, size);
 }
 
-
-int main(int argc, char **argv){
-
+int create_server_socket() {
     int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (sock_fd < 0) {
         std::cerr << "Failed to create socket\n";
+        exit(1);
     }
 
     struct sockaddr_in server_addr;
@@ -31,38 +31,87 @@ int main(int argc, char **argv){
 
     if (bind(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
         std::cerr << "Failed to Bind: " << strerror(errno) << "\n";
+        exit(1);
     }
 
     if (listen(sock_fd, BACKLOG) == -1) {
         std::cerr << "Listen Failed: " << strerror(errno) << "\n";
+        exit(1);
     }
 
+    return sock_fd;
+}
 
+int accept_connection(int sock_fd) {
     struct sockaddr_in client_addr;
     int client_size = sizeof(client_addr);
 
     int client_fd = accept(sock_fd, (struct sockaddr*)&client_addr, (socklen_t*)&client_size);
     std::cout << "Client connected\n";
 
+    return client_fd;
+}
 
-
+std::string receive_request(int client_fd) {
     char buf[100];
-    int bytes_recieved = recv(client_fd, buf, 99, 0);
+    int bytes_received = recv(client_fd, buf, 99, 0);
 
 
-    buf[bytes_recieved] = '\0'; //Adds null terminator to buffer
-    std::cout << "Bytes received: " << bytes_recieved<< "\n";
+    buf[bytes_received] = '\0'; //Adds null terminator to buffer
     std::cout << "Client sent:\n" << buf << "\n";
+    return std::string(buf, bytes_received);
+}
 
-    std::string request = messageStr(buf, 100);
+std::unordered_map<std::string, std::string> parse(std::string request) {
+    std::unordered_map<std::string, std::string> parsed_response;
+    std::string line;
+    std::istringstream request_stream(request);
+    
+    getline(request_stream, line);
+
+    while (getline(request_stream, line)) {
+        int split_location = line.find(':');
+        if (split_location != std::string::npos) {
+            std::string key = line.substr(0, split_location);
+            std::string value = line.substr(split_location + 2);
+
+            parsed_response[key] = value;
+        }
+    }
+    // for (const auto& pair : parsed_response) {
+    //     std::cout << "Key: " << pair.first << " Value: " << pair.second << "\n";
+    // }
+    return parsed_response;
+
+}
+
+int main(int argc, char **argv){
+    int sock_fd = create_server_socket();
+    int client_fd = accept_connection(sock_fd);
+   
+    std::string request = receive_request(client_fd);
     std::istringstream request_stream(request);
     std::string method, path, version;
     request_stream >> method >> path >> version;
 
-    std::cout << "Path: " << path << "\n";
+    std::cout << "Path: " << path << "\n"; 
 
-    if (path == "/") {
+    std::unordered_map<std::string, std::string> parsed_resp = parse(request); 
+
+    if (path == "/" && parsed_resp.count("User-Agent") == 0) {
         std::string aprove_res = "HTTP/1.1 200 OK\r\n\r\n";
+        send(client_fd, aprove_res.c_str(), aprove_res.size(), 0);
+    } else if (path.find("/echo/") != std::string::npos){
+        std::string aprove_res = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " 
+                                + std::to_string(path.length() - 6) 
+                                + "\r\n\r\n" 
+                                + path.substr(6);
+        send(client_fd, aprove_res.c_str(), aprove_res.size(), 0);
+    } else if (parsed_resp.count("User-Agent") != 0) {
+        std::string aprove_res = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " 
+                                + std::to_string(parsed_resp["User-Agent"].length()) 
+                                + "\r\n\r\n" 
+                                + parsed_resp["User-Agent"];
         send(client_fd, aprove_res.c_str(), aprove_res.size(), 0);
     } else {
         std::string aprove_res = "HTTP/1.1 404 Not Found\r\n\r\n";
