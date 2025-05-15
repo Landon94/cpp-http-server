c2a9bffcbdbc16d7c1bf515c17d0e34700f481b9
Landon94 <lkarkut@uci.edu>
2025-05-15 14:56:38 -0700

    Threadpool.cpp now only implements threadpool class methods.

--- src/threadpool.cpp
+++ src/threadpool.cpp
@@ -3,60 +3,50 @@
 #include <mutex>
 #include <queue>
 #include <thread>
+#include "threadpool.h"
 
-class ThreadPool
-{
-private:
-    std::mutex queue_mutex;
-    std::vector<std::thread> workers;
-    std::queue<std::function<void()>> tasks;
-    std::condition_variable condition;
-    bool shutdown = false;
-    
-public:
-    ThreadPool(size_t num_threads) {
-        for (size_t i = 0; i < num_threads; i++) {
-            workers.emplace_back([this] { //creates new worker 
-                for(;;) {
-                    std::unique_lock<std::mutex> lock(queue_mutex);
+ThreadPool::ThreadPool(size_t num_threads) {
+    for (size_t i = 0; i < num_threads; i++) {
+        workers.emplace_back([this] { //creates new worker which waites for tasks
+            for(;;) {
+                std::unique_lock<std::mutex> lock(queue_mutex);
 
-                    //locks tasks to single thread. Puts threads to sleep if no tasks or shutingdow
-                    condition.wait(lock, [this] {return !tasks.empty() || shutdown; });
+                //locks tasks to single thread. Puts threads to sleep if no tasks or shutingdow
+                condition.wait(lock, [this] {return !tasks.empty() || shutdown; });
 
-                    if (tasks.empty() && shutdown) {
-                        return;
-                    }
-
-                    auto task = std::move(tasks.front()); //takes task from front of the queue
-                    tasks.pop();
-
-                    lock.unlock();
-                    task();
+                if (tasks.empty() && shutdown) {
+                    return;
                 }
-            });
-        }
+
+                auto task = std::move(tasks.front()); //takes task from front of the queue
+                tasks.pop();
+
+                lock.unlock();
+                task();
+            }
+        });
     }
+}
     
-    ~ThreadPool() {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        shutdown = true;
-        lock.unlock();
-        condition.notify_all(); //Wakes up all threads
+ThreadPool::~ThreadPool() {
+    std::unique_lock<std::mutex> lock(queue_mutex);
+    shutdown = true;
+    lock.unlock();
+    condition.notify_all(); //Wakes up all threads
 
-        for (auto& worker: workers) {
-            worker.join(); //Waits for workers to finish task
-        }
+    for (auto& worker: workers) {
+        worker.join(); //Waits for workers to finish task
     }
+}
 
-    void enqueue(std::function<void()> task) {
-        {
-            std::unique_lock<std::mutex> lock(queue_mutex); //Locks tasks_queue so only single thread can modify it
-            tasks.emplace(std::move(task)); //task is added to tasks queue
-        } 
+void ThreadPool::enqueue(std::function<void()> task) {
+    {
+        std::unique_lock<std::mutex> lock(queue_mutex); //Locks tasks_queue so only single thread can modify it
+        tasks.emplace(std::move(task)); //task is added to tasks queue
+    } 
 
-        condition.notify_one(); //notifies single worker thread of new task if any
-    }
-};
+    condition.notify_one(); //notifies single worker thread of new task if any
+}
 
 
 
